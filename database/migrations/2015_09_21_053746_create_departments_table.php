ee7a01b0d79755d0f37005a3b3296b96983c9399
sunhuang <sunhuang2005@gmail.com>
2015-09-21 13:41:04 +0800

    ACE Template

---
+++ database/migrations/2015_09_21_053746_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned()->index();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->string('name');
+            $table->string('costcenter');
+            $table->timestamps();
+            $table->unique(array('company_id','name','costcenter'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('departments');
+    }
+}
